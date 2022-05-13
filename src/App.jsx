c1853af0db71c771fe92ab6364c8891d4f67a732
Cissy <91531100+cissyverhaeghe@users.noreply.github.com>
2022-05-13 13:14:13 +0200

    style

--- src/App.jsx
+++ src/App.jsx
@@ -11,42 +11,41 @@ const App = () => {
 
   return (
     <>
-      <h1>Cocktails</h1>
-      <input
-        type="text"
-        placeholder="find your cocktail"
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-      />
-      {isError && <p>er is een error</p>}
-      {isLoading && <p>Loading...</p>}
-      <div className="cards">
-        {data &&
-          data.drinks.map(({ strDrink, idDrink, strDrinkThumb }) => (
-            <div className="card" key={idDrink}>
-              <img
-                className="card-img-top"
-                src={strDrinkThumb}
-                alt="Card image cap"
-              />
-              <div className="card-body">
-                <h5 className="card-title">{strDrink}</h5>
-                <p className="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-                <a
-                  href="#"
-                  className="btn btn-primary"
-                  onClick={() => setCocktailId(idDrink)}
-                >
-                  Details
-                </a>
+      <section>
+        <h1>Cocktails</h1>
+
+        <input
+          type="text"
+          placeholder="find your cocktail"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        {isError && <p>er is een error</p>}
+        {isLoading && <p>Loading...</p>}
+        <div className="cards">
+          {data &&
+            data.drinks.map(({ strDrink, idDrink, strDrinkThumb }) => (
+              <div className="card" key={idDrink}>
+                <img
+                  className="card-img-top"
+                  src={strDrinkThumb}
+                  alt="Card image cap"
+                />
+                <div className="card-body">
+                  <h5 className="card-title">{strDrink}</h5>
+                  <a
+                    href="#"
+                    className="btn btn-primary"
+                    onClick={() => setCocktailId(idDrink)}
+                  >
+                    Details
+                  </a>
+                </div>
               </div>
-            </div>
-          ))}
-      </div>
-      {<CocktailDetails id={cocktailId} />}
+            ))}
+        </div>
+      </section>
+      <aside>{<CocktailDetails id={cocktailId} />}</aside>
     </>
   );
 
